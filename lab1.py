389047814d366eb1e38dd7b6ea0eb59ebc15052f
annabsmirnova <90455502+annabsmirnova@users.noreply.github.com>
2021-12-02 23:18:26 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,34 +1,37 @@
 import json
 import re
 
-f = open('pokemon_full.json')
-file = f.read()
-print('Общее количество символов в файле - ', len(file))
+file = open('pokemon_full.json')
+anime_str = file.read()
+print('Общее количество символов в файле - ', len(anime_str))
 
-file_without_punctuation = re.sub(r'[^-\w]','', file) #Я считала, что дефис это не знак препинания, в файле (-) используется как дефис
-print('Количесвто символов в файле без учета пробелов и знаков препинания - ', len(file_without_punctuation))
+anime_str_without_punctuation = re.sub(r'[^-\w]','', anime_str)
+#Я считала, что дефис это не знак препинания.
+#В файле (-) используется как дефис
+print('Количесвто символов в файле '
+      'без учета пробелов и знаков препинания - ',
+      len(anime_str_without_punctuation))
 
-f.seek(0)
-file = json.load(f)
-f.close()
-m = 0
-n = len(file)
-for i in range(0, n):
-    if m < len(file[i]['description']):
-        m = len(file[i]['description'])
-        max_name = file[i]['name']
-print('Имя покемона с самым длинным описанием - ', max_name)
-
-max_slov = 0
+file.seek(0)
+anime_str = json.load(file)
+file.close()
+max_name_len = 0
+len_anime_str = len(anime_str)
+max_words = 0
 all_max_ability = []
-for i in range(0, n):
-    abilities = file[i]['abilities']
+for pokemon in range(len_anime_str):
+    description_len = len(anime_str[pokemon]['description'])
+    if max_name_len < description_len:
+        max_name_len = description_len
+        max_name = anime_str[pokemon]['name']
+    abilities = anime_str[pokemon]['abilities']
     for one_ability in abilities:
-        if max_slov < len(one_ability.split()):
+        abilitiy_len = len(one_ability.split())
+        if max_words < abilitiy_len:
             max_ability = one_ability
-            max_slov = len(one_ability.split())
+            max_words = abilitiy_len
             all_max_ability = [one_ability]
-        elif max_slov == len(one_ability.split()):
+        elif max_words == abilitiy_len:
             all_max_ability.append(one_ability)
+print('Имя покемона с самым длинным описанием - ', max_name)
 print('Умение, которое содержит больше всего слов - ', *all_max_ability)
-
